333b01ef27b5153966815aadbe89db41af15ff79
johnschiemanjr <johnschiemanjr@gmail.com>
2021-01-18 23:55:26 -0500

    Began frontend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,20 +12,24 @@ app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
 
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
 const connection = mongoose.connection;
 
-connection.once('open', () => {
-	console.log('MongoDB connection successfully established.');
+connection.once("open", () => {
+    console.log("MongoDB connection successfully established.");
 });
 
-const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
 
 // Adding routers...by adding /users to the URL we can use the User router as an API endpoint
-app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
 
-app.listen(port, () =>{
-	console.log('Server is running on port: ' + port);
+app.listen(port, () => {
+    console.log("Server is running on port: " + port);
 });
